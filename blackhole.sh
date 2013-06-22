d17b1b24c4e8b74c0d26ce8e3e768c02590c676f
yihua.huang <code4crafter@gmail.com>
2013-06-22 18:51:25 +0800

    update scriptes

--- blackhole.sh
+++ blackhole.sh
@@ -3,6 +3,25 @@ HOME_DIR=/usr/local/blackhole
 PATH=$PATH:$HOME_DIR
 export PATH
 
+function doCache(){
+  case "$1" in
+      stat)
+        java -jar $HOME_DIR/lib/wifesays-1.0.0-alpha.jar -cstat_cache
+        ;;
+      dump)
+        echo "Dump cache to $HOME_DIR/cache.dump"
+        java -jar $HOME_DIR/lib/wifesays-1.0.0-alpha.jar -cdump_cache > /dev/null
+        ;;
+      clear)
+        echo "Clear cache"
+        java -jar $HOME_DIR/lib/wifesays-1.0.0-alpha.jar -cclear_cache
+        ;;
+      *)
+    echo "Usage: $0 cache {dump|clear|stat}"
+    ;;
+  esac
+}
+
 case "$1" in
   start)
     echo "Starting blackhole..."
@@ -12,6 +31,9 @@ case "$1" in
     echo "Stopping blackhole"
     java -jar $HOME_DIR/lib/wifesays-1.0.0-alpha.jar -cshutdown > /dev/null
     ;;
+  cache)
+    doCache $2
+    ;;  
   restart)
     echo "Stopping blackhole..."
     java -jar $HOME_DIR/lib/wifesays-1.0.0-alpha.jar -cshutdown > /dev/null;
@@ -32,7 +54,7 @@ case "$1" in
     java -jar $HOME_DIR/lib/wifesays-1.0.0-alpha.jar -creload > /dev/null
     ;;
   *)
-    echo "Usage: $0 {start|stop|reload|zones|config|restart}"
+    echo "Usage: $0 {start|stop|reload|zones|config|restart|cache}"
     ;;
 esac
 
